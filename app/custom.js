9a1ed4e1a7279ed0d70da5a96eee5312eadaa53e
chris@chriszimmerman.net <chris@chriszimmerman.net>
2017-04-29 08:35:09 -0400

    Fixes spacing

--- app/custom.js
+++ app/custom.js
@@ -1,16 +1,16 @@
 $(document).ready(function () {
-    $('#favorites').hide().addClass('hidden');
-    $('#home').addClass('visible');
+  $('#favorites').hide().addClass('hidden');
+  $('#home').addClass('visible');
 
-    $('.same-page-nav').click(function (event) {
-        var target = $(event.target);
+  $('.same-page-nav').click(function (event) {
+    var target = $(event.target);
 
-        if (target.is('a')) {
-            event.preventDefault();
-            if ($(target.attr('href')).hasClass('hidden')) {
-                $('.visible').removeClass('visible').addClass('hidden').hide();
-                $(target.attr('href')).removeClass('hidden').addClass('visible').show();
-            }
-        }
-    });
+    if (target.is('a')) {
+      event.preventDefault();
+      if ($(target.attr('href')).hasClass('hidden')) {
+        $('.visible').removeClass('visible').addClass('hidden').hide();
+        $(target.attr('href')).removeClass('hidden').addClass('visible').show();
+      }
+    }
+  });
 });
